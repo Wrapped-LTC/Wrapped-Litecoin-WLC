b37d7e92c1cb4221e65ddba632b451e0bf6ce670
wedergarten <62032531+wedergarten@users.noreply.github.com>
2021-03-25 18:26:39 -0400

    Update WrappedLitecoinContract.sol

--- WrappedLitecoinContract.sol
+++ WrappedLitecoinContract.sol
@@ -1,110 +1,104 @@
 pragma solidity ^0.7.5;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
-import "https://github.com/smartcontractkit/chainlink/blob/develop/evm-contracts/src/v0.7/interfaces/AggregatorV3Interface.sol";
 
 contract WrappedLitecoin {
     uint256 public TokenCap;
-    uint256 private TotalSupply;
+    uint256 public TotalSupply;
+    uint256 public burntSupply;
     string public name;
     string public symbol;
     uint8 public decimals;
-    address private owner;
-    uint256 private MaxBurntSupply;
-    uint256 public BurntSupply;
+    address private ownerAddy;
     address private ZeroAddress;
-    AggregatorV3Interface internal priceFeed;
     //variable Declarations
     
+      
+    event Transfer(address indexed from, address indexed to, uint256 value);    
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    //Event Declarations 
+    
+    using SafeMath for uint256;
+    
+    mapping(address => uint256) balances;
+
+    mapping(address => mapping (address => uint256)) allowed;
     
     
-    mapping (address => uint256) private AddressBal;
     
-    
-    constructor(uint256 _TokenCap, uint256 _TokenSupply, string memory _name, string memory _symbol, uint8 _decimals,address _OwnerAddress, uint256 _MaxBurn){
+    constructor(uint256 _TokenCap, uint256 _TokenSupply, string memory _name, string memory _symbol, uint8 _decimals){
     TokenCap = _TokenCap;
     TotalSupply = _TokenSupply;
     name = _name;
     symbol = _symbol;
     decimals = _decimals;
-    MaxBurntSupply = _MaxBurn;
-    owner = _OwnerAddress;
-    priceFeed = AggregatorV3Interface(0x4d38a35C2D87976F334c2d2379b535F1D461D9B4);
-    //Deployment Constructor
+    ownerAddy = msg.sender;
+    //Deployment Constructors
     }
     
-    //functions
     
-    function totalSupply() public view returns(uint256){
-        return TotalSupply;
-    }
+
     
-    function balanceOf(address _owner) public view returns (uint256 balance){
-        return AddressBal[_owner];
-    }
-    
-    function transfer(address _to, uint256 _value) public returns(bool success){
-        require (AddressBal[msg.sender] >= _value);
-        AddressBal[msg.sender] -= _value;
-        AddressBal[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
-        return success;
-    }
-    
-    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
-        require (AddressBal [_from] >= _value);
-        require (msg.sender == owner);
-        AddressBal [_from] -= _value;
-        AddressBal [_to] += _value;
-        emit Transfer(_from, _to, _value);
-        return success;
-        
-    }
-        
-    function approve(address _spender, uint256 _value) public returns (bool success){
-        emit Approval(owner, _spender, _value);
-        return success;
-        
-    }
-    
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining){
-        
+    function balanceOf(address Address) public view returns (uint256 balance){
+        return balances[Address];
+
     }
 
-        
+    function approve(address delegate, uint numTokens) public returns (bool) {
+        allowed[msg.sender][delegate] = numTokens;
+        emit Approval(msg.sender, delegate, numTokens);
+        return true;
+    }
+    //Approves an address to spend your coins
+
+    function transferFrom(address owner, address buyer, uint numTokens) public returns (bool) {
+        require(numTokens <= balances[owner]);    
+        require(numTokens <= allowed[owner][msg.sender]);
+    
+        balances[owner] = balances[owner].sub(numTokens);
+        allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
+        balances[buyer] = balances[buyer].add(numTokens);
+        emit Transfer(owner, buyer, numTokens);
+        return true;
+    }
+    //Transfer From an other address
+
+
     function Mint(address _MintTo, uint256 _MintAmount) public {
-        require (msg.sender == owner);
-        require (TotalSupply + _MintAmount <= TokenCap);
-        AddressBal[_MintTo] += _MintAmount;
-        TotalSupply += _MintAmount;
+        require (msg.sender == ownerAddy);
+        require (TotalSupply.add(_MintAmount) <= TokenCap);
+        balances[_MintTo] = balances[_MintTo].add(_MintAmount);
+        TotalSupply = TotalSupply.add(_MintAmount);
         ZeroAddress = 0x0000000000000000000000000000000000000000;
         emit Transfer(ZeroAddress ,_MintTo, _MintAmount);
     }
-    
-    function Burn(uint256 _BurnAmount) public {
-        require (AddressBal [msg.sender] >= _BurnAmount);
-        require (MaxBurntSupply >= BurntSupply + _BurnAmount);
-        AddressBal[msg.sender] -= _BurnAmount;
-        TotalSupply -= _BurnAmount;
-        BurntSupply += _BurnAmount;
-        emit Transfer(msg.sender, owner, _BurnAmount);
-        
-    }
-    
-    function GetLTCPrice() external view returns(int256){
-     (, int256 answer,,,) = priceFeed.latestRoundData();
-    return answer;
-    }
-        
-     //Event Declarations   
-    event Transfer(address indexed from, address indexed to, uint256 value);    
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-    
+    //Mints tokens to your address 
 
-    
-    //
-    
-    
+
+    function Burn(uint256 _BurnAmount) public {
+        require (balances[msg.sender] >= _BurnAmount);
+        balances[msg.sender] = balances[msg.sender].sub(_BurnAmount);
+        burntSupply = burntSupply.sub(_BurnAmount);
+        ZeroAddress = 0x0000000000000000000000000000000000000000;
+        emit Transfer(msg.sender, ZeroAddress, _BurnAmount);
+        
+    }
+
+
+
+
+
 }
 
+library SafeMath { 
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+      assert(b <= a);
+      return a - b;
+    }
+    
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+      uint256 c = a + b;
+      assert(c >= a);
+      return c;
+    }
 
+}
